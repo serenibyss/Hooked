13f49da33e6c377b711856405186f8812c075034
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-08-03 01:31:00 -0500

    build, cleanout stuff

--- src/main/java/thecodewarrior/hooked/common/util/Utils.kt
+++
@@ -1,7 +0,0 @@
-package thecodewarrior.hooked.common.util
-
-fun Double.finiteOrDefault(defaultValue: Double) = if(this.isFinite()) this else defaultValue
-fun Float.finiteOrDefault(defaultValue: Float) = if(this.isFinite()) this else defaultValue
-
-fun Double.finiteOrDefault(defaultValue: () -> Double) = if(this.isFinite()) this else defaultValue()
-fun Float.finiteOrDefault(defaultValue: () -> Float) = if(this.isFinite()) this else defaultValue()
